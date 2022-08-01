5ee028695ba07d4c4f1a02a7a929278c614fea6b
Milan Contreras <milan.contreras@epn.edu.ec>
2022-08-01 09:22:38 -0500

    Header and Definitios

---
+++ 3DEnvironment/Definitions.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+//---------------------------------------------------------------------------
+#ifndef DefinitionsH
+#define DefinitionsH
+
+//Projection-----------------------------------------------------------------
+#define xy                      0
+#define xz                      1
+#define yz                      2
+
+//Constants------------------------------------------------------------------
+#define NUM_COLORS          4       //Numero de colores para el mapa de calor
+#define SPEED               0.1     //Velocidad de animaci�n
+#define PI                  3.1415926535897932384626433832795
+#define MaxNPoints          200     //maximum Number of points
+#define MAX_NUM_REFLECTIONS 50
+#define NUMRAYS				1000
+#define SECURITYARRAYSPACE	10
+#define NUMRAYDRAW			1002
+//---------------------------------------------------------------------------
+#endif
